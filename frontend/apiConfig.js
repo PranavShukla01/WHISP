d0c2f3aa858044e2be89c77b01b93e10ee906cbf
PranavShukla01 <143289933+PranavShukla01@users.noreply.github.com>
2025-07-03 00:56:02 +0530

    Update apiConfig.js

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -3,11 +3,11 @@ let baseUrl;
 let socketUrl;
 
 if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "https://simple-chat-app-6yeb.onrender.com";
-  socketUrl = "wss://simple-chat-app-6yeb.onrender.com";
+  baseUrl = "https://whisp-yq5j.onrender.com";
+  socketUrl = "wss://whisp-yq5j.onrender.com";
 } else {
   baseUrl = "https://whisp-yq5j.onrender.com";
-  socketUrl = "wss://simple-chat-app-6yeb.onrender.com";
+  socketUrl = "wss://whisp-yq5j.onrender.com";
 }
 
 export { baseUrl, socketUrl };
